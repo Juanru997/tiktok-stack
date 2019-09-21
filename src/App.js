9187e2d537d18e628444544de8aefb0cdce05c01
Juanru <yz5026@nyu.edu>
2019-09-21 15:11:48 +0800

    migrate html, css from former assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div id="parent">
+<div className="all-content">
+
+    <div class="ur_code">
+      <h2 class="through">TikTok is Recording Your Wonderful Life<br></br></h2>
+      <h2>TikTok Destroys Your Wonderful Real Life</h2>
     </div>
+
+
+    <div className="stats">
+      <p>
+        Addiction to TikTok is quite common for many users in different ages. <br></br>According to the report of Tiktok users research conducted by Tencent,
+        the average user open the app about 14 days per month and 22% of users use the apps every day for more than 1
+        hour.
+      </p>
+      </div>
+
+      <h1>The Stack of TikTok</h1>
+
+      <div class="stackInfo">
+        <h3>User Layer</h3>
+      </div>
+      <img alt="User layer" src="https://i.ibb.co/x5qVytp/2019-09-17-10-40-03.png" width="800" height="600" />
+
+      <div class="stackInfo">
+        <h3>Interface Layer</h3>
+      </div>
+      <img alt="Interface layer" src="http://2eo55h24wdu6nyeph4frss2b76-wpengine.netdna-ssl.com/wp-content/uploads/2019/03/tiktok_1.jpg" width="800" height="600" />
+
+      <div class="stackInfo">
+        <h3>Address Layer</h3>
+      </div>
+      <img alt="Address layer" src="https://cdn1.imggmi.com/uploads/2019/9/17/d48d72c8fa5b5978023033da2f85121a-full.jpg" width="800" height="600" />
+
+      <div class="stackInfo">
+        <h3>City Layer</h3>
+      </div>
+      <img alt="City layer" src="https://i.ibb.co/cvvQtpG/City.png" width="800" height="600" />
+
+      <div class="stackInfo">
+        <h3>Cloud Layer</h3>
+      </div>
+      <img alt="Cloud layer" src="https://cdn1.imggmi.com/uploads/2019/9/17/252cfa9a096948774b11664818a6bec7-full.png" width="800" height="600" />
+
+      <div class="stackInfo">
+        <h3>Earth Layer</h3>
+      </div>
+      <img alt="Earth layer" src="https://i.ibb.co/vB4ZKdJ/earth-layer.png" width="800" height="600" />
+
+    </div>
+    </div>
+
+
+   
+
   );
 }
 
